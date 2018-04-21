2b0102c2bc34a49d63036595551cbcb4262b59a5
danivsainz <danivsainz@gmail.com>
2018-04-21 06:20:28 -0400

    code . --disable-gpu

--- client/src/App.vue
+++ client/src/App.vue
@@ -11,6 +11,7 @@ export default {
 </script>
 
 <style>
+@import '../node_modules/bootstrap/scss/bootstrap.scss';
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
