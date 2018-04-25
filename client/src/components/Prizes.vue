645b9c3ad0590c4ac3fdaddbca9eb76634eae19f
danivsainz <danivsainz@gmail.com>
2018-04-24 22:17:56 -0400

    add conditional styling

--- client/src/components/Prizes.vue
+++ client/src/components/Prizes.vue
@@ -8,14 +8,15 @@
 
     <div class="row">
       <div class="col-xlg-8 col-xl-8 col-lg-12 col-sm-12 col-12 center gray">
-        <b-card-group columns>
-          <b-card v-for="prize in prizes"  :key="prize._id" :title="`Win a ` + prize.name"
+        <b-card-group columns class="myCardGroup">
+          <b-card v-for="(prize, index) in prizes"  :key="prize._id" :title="`Win a ` + prize.name"
             :img-src='prize.image_url'
             img-alt="Image"
             img-top
             tag="article"
             class="mb-3 my-card"
             style='border:none;'
+            v-bind:class="{'conditional-margin': needsMargin(index+2)}"
           >
             <div class="flex-justify"  style='height:105px;'>
               <router-link v-bind:to="'prizes/' + prize._id">
@@ -38,7 +39,8 @@ export default {
   name: 'Prizes',
   data () {
     return {
-      prizes: []
+      prizes: [],
+      margin: null
     }
   },
   mounted () {
@@ -52,6 +54,13 @@ export default {
     async deletePost (id) {
       await PrizesService.deletePost(id)
       this.getPosts()
+    },
+    needsMargin (index) {
+      if(index % 3 == 0){
+        return true
+      }else{
+        return false
+      }
     }
   }
 }
@@ -94,6 +103,15 @@ p{
 .card img{
   border-top-left-radius: calc(0.45rem - 1px);
   border-top-right-radius: calc(0.45rem - 1px);
+}
+.myCardGroup{
+  flex-flow: wrap;
+  display: flex;
+  justify-content: center;
+}
+.conditional-margin{
+  margin-left: 15px;
+  margin-right:15px;
 }
 
 @media screen and (max-width: 1414px) {
